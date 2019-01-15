9034d992536e53e71485242061bfac503edda377
uralmetal <uralmetal1@yandex.ru>
2019-01-15 18:39:06 +0300

    add targets to gitignore and main header, source

---
+++ ft_printf.c
@@ -0,0 +1,5 @@
+//
+// Created by Rickard thorne Walder haigh on 2019-01-15.
+//
+
+#include "ft_printf.h"
---
+++ ft_printf.h
@@ -0,0 +1,8 @@
+//
+// Created by Rickard thorne Walder haigh on 2019-01-15.
+//
+
+#ifndef FT_PRINTF_FT_PRINTF_H
+#define FT_PRINTF_FT_PRINTF_H
+
+#endif //FT_PRINTF_FT_PRINTF_H
