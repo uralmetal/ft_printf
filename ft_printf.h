95bda0e1393c94b7111c646907ec7c74b1e17eea
uralmetal <uralmetal1@yandex.ru>
2019-01-16 10:31:32 +0300

    add head 42 in ft_printf and add libft

--- ft_printf.h
+++ ft_printf.h
@@ -1,8 +1,20 @@
-//
-// Created by Rickard thorne Walder haigh on 2019-01-15.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwalder- <rwalder-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/16 10:23:06 by rwalder-          #+#    #+#             */
+/*   Updated: 2019/01/16 10:23:14 by rwalder-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef FT_PRINTF_FT_PRINTF_H
-#define FT_PRINTF_FT_PRINTF_H
+#ifndef _FT_PRINTF_H
+#define _FT_PRINTF_H
 
-#endif //FT_PRINTF_FT_PRINTF_H
+#include "ft_printf.h"
+
+void ft_printf(char *s);
+
+#endif //_FT_PRINTF_H
