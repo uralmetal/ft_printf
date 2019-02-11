04ef22c41c74191e61649ce08a48dca1c6c53b44
gleonett <alex.zybov95@gmail.com>
2019-02-11 11:21:13 +0300

    fixed bugs {%S}

--- print/choose_arg.c
+++ print/choose_arg.c
@@ -6,7 +6,7 @@
 /*   By: gleonett <gleonett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 18:39:26 by gleonett          #+#    #+#             */
-/*   Updated: 2019/02/10 15:07:13 by gleonett         ###   ########.fr       */
+/*   Updated: 2019/02/11 11:11:35 by gleonett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ char	*choose_arg_prec_0(const void **arg, t_get_output function_get,
 	char *output;
 
 	output = NULL;
-	if ((var_d != 0.0 || var_d == (-0.0)) && var_dd == 0)
+	if ((var_d != 0.0 || var_d == (-0.0)) && g_mod->type != 52)
 		if (g_mod->type == 56 || g_mod->type == 57 || g_mod->type == 59 ||
 		g_mod->type == 60)
 			output = function_get(var_d, -1);
 		else
 			output = function_get(var_d, 6);
-	else if (var_dd != 0 || (var_dd == -0.0 && var_d == 0 && *arg == 0 &&
+	else if (var_dd != 0 || (var_dd == -0.0 && var_d == 0 && arg == 0 &&
 	g_mod->type != 1 && g_mod->type != 52))
 		if (g_mod->type == 58 || g_mod->type == 61)
 			output = function_get(var_dd, -1);
@@ -41,7 +41,7 @@ char	*choose_arg_prec(const void **arg, t_get_output function_get,
 	char *output;
 
 	output = NULL;
-	if (var_d != 0.0 || var_d == (-0.0))
+	if ((var_d != 0.0 || var_d == (-0.0)) && g_mod->type != 52)
 		output = function_get(var_d, g_mod->precision);
 	else if (var_dd != 0 || (var_dd == -0 && var_d == 0 && *arg == 0 &&
 			g_mod->type != 1 && g_mod->type != 52))
